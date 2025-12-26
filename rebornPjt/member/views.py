18d7bf637c3c51754b7b8f163e3c1a40ea2c0f83
aeo <hjeei1128@naver.com>
2025-12-26 18:24:47 +0900

    회원

--- rebornPjt/member/views.py
+++ rebornPjt/member/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def blist(request):
-    return render(request,'board/blist.html')
+def login(request):
+    return HttpResponse("hi")
