7ce28f863591c1096858608721cfefc96724e5ec
m.vagga <m.vagga@gmail.com>
2025-12-29 12:30:11 +0900

    magazine

--- rebornPjt/magazine/models.py
+++ rebornPjt/magazine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Magazine(models.Model):
+    mno = models.AutoField(primary_key=True)
+    mtitle = models.CharField(max_length=1000)
+    mcontent = models.TextField()
+    mtype = models.CharField(max_length=2)
+    mfile = models.FileField(default='',null=True)
+    mhit = models.IntegerField(default=0)
+    mdate = models.DateTimeField(auto_now=True)
+    
+    
