e0e806fd610829688c1efac08e224ee3699ef43e
m.vagga <m.vagga@gmail.com>
2025-12-29 17:06:24 +0900

    magazine

--- rebornPjt/magazine/views.py
+++ rebornPjt/magazine/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from magazine.models import Magazine, Magazine_code
 
 def mlist(request):
-    return render(request,'magazine/mlist.html')
+    
+    # 매거진 코드 정보
+    qs_code = Magazine_code.objects.all()
+
+    # 매거진 리스트
+    qs = Magazine.objects.all().order_by('-mdate')
+
+    page = int(request.GET.get('page',1))
+    paginator = Paginator(qs,10)
+    qs_list = paginator.get_page(page)
+
+
+    context = {'qs_code':qs_code,'list':qs_list,'page':page}
+    return render(request,'magazine/mlist.html',context)
 
 
