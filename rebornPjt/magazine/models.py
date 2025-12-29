9379228e84cabeec2c20c43b9fd6b3ad4a0c4bbf
m.vagga <m.vagga@gmail.com>
2025-12-29 13:15:44 +0900

    restaurants

--- rebornPjt/magazine/models.py
+++ rebornPjt/magazine/models.py
@@ -16,11 +16,11 @@ class Magazine(models.Model):
     mtitle = models.CharField(max_length=1000)
     mcontent = models.TextField()
     magazine_code = models.ForeignKey(Magazine_code,on_delete=models.SET_NULL,null=True)
-    mfile = models.FileField(default='',null=True)
+    mfile = models.FileField(default='',null=True,blank=True)
     mhit = models.IntegerField(default=0)
     mdate = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f'{self.mno},{self.mtitle}'    
+        return f'{self.mno},{self.mtitle}'
     
 
--- rebornPjt/restaurants/admin.py
+++ rebornPjt/restaurants/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from restaurants.models import Restaurants
 
 admin.site.register(Restaurants)
+
